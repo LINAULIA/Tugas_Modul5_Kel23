12d6889e70224f246f61320ee944a0a0e25475ad
wwiiinn11 <75421679+wwiiinn11@users.noreply.github.com>
2021-09-23 12:56:50 +0700

    update laptop

--- src/Component/laptop.js
+++ src/Component/laptop.js
@@ -1,85 +1,59 @@
-import { useContext, createContext, useState, useEffect } from "react";
-
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import CardMedia from "@material-ui/core/CardMedia";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./laptop.css";
+import style from "./laptop.css";
+// import Modal from '@mui/material/Modal';
+// import Box from '@mui/material/Box';
 
-const styles = makeStyles((theme) => ({
-  slider: {
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-  },
-  paper: {
-    margin: theme.spacing(2),
-    justifyContent: "flex-end",
-    textAlign: "center",
-    paddingBottom: theme.spacing(2),
-    backgroundColor: "white",
-    ["@media (max-width:600px)"]: {
-      flexDirection: "column",
-    },
-  },
-  label: {
-    textTransform: "none",
-  },
-  grid: {
-    display: "flex",
-    justifyContent: "flex-start",
-    backgroundColor: "white",
-    direction: "row",
-    margin: "auto",
-    marginBottom: theme.spacing(2),
-    ["@media (max-width:600px)"]: {
-      flexDirection: "column",
-    },
-  },
-  button: {
-    padding: "5%",
-    "&:hover": {
-      color: "#4f25c8",
-    },
-  },
-}));
-export default function Handphone() {
-  const classes = styles();
-  const [valueInput, setValueInput] = useState({
-    name: "",
-  });
 
-  const handleIdPembelian = (event, type) => {
-    if (type === "name") {
-      setValueInput((prevState) => {
-        return { ...prevState, name: event.target.value };
-      });
-    }
-  };
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("  http://localhost:3333/data")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  return (
-    <div>
-      <marquee style={{ fontWeight: "bold" }} bgcolor="FFF300" align ="center" direction ="left" scrollamount="10"> SELAMAT DATANG DI L&W ELECTRONIC, PUSAT STORE HP DAN LAPTOP TERBESAR, DAN TERMURAH SEJAGAT RAYA </marquee>
-      <div style={{ marginTop: 20 }}>
+export default class Handphone extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+			recipe: [],
+			visible: false,
+		};
+	}
+
+	handleButton = (steps) => {
+		alert("Spesifikasi : " + steps);
+	};
+	
+	componentDidMount() {
+		axios({
+			method: "get",
+			url: "http://localhost:3000/data",
+			headers: {
+				accept: "*/*",
+			},
+		})
+		.then((data) => {
+			console.log(data.data);
+			this.setState({
+				recipe: data.data,
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	}
+	
+	render() {
+		return (
+			<div style={{ backgroundColor: "#845EC2" }}>
+        <marquee style={{ fontWeight: "bold" }} bgcolor="FFF300" align ="center" direction ="left" scrollamount="10"> SELAMAT DATANG DI L&W ELECTRONIC, PUSAT STORE HP DAN LAPTOP TERBESAR, DAN TERMURAH SEJAGAT RAYA </marquee>
+        <div style={{ marginTop: 20 }}>
       <center>
       <input className="search"
-              onChange={(event) => handleIdPembelian(event, "name")}
-              name="idPembelian"
-              value={valueInput.name}
               style={{ color: "#6e0234"}}
               placeholder="Masukkan Nama Merk"
             />
@@ -87,36 +61,58 @@ export default function Handphone() {
       <FontAwesomeIcon icon={faSearch} />
      </button>
             </center>
-            <div className="marquee" >
+        <div className="marquee" >
             <center>
-              <h3> ... Mencari data {valueInput.name} ...</h3>
+              <h3> ... Mencari data ...</h3>
         </center>
         </div>
-        <Grid container md={11} spacing={4} className={classes.grid}>
-          {data.map((value) => (
-            <Grid item key={value.name} md={3}>
-              <Card className={classes.paper}>
-                <CardActionArea>
-                  <CardMedia
-                    style={{
-                      height: "150px",
-                      margin: "auto",
-                      paddingTop: "5%",
-                    }}
-                    component="img"
-                    className={classes.media}
-                    image={value.lurl}
-                  />
-                  <CardContent>
-                  <Typography style={{fontWeight:"bold"}}>{value.lname}</Typography>
-                    <Typography>Harga : {value.lprice} </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+				<Grid container
+                    md={11}
+                    spacing={4}
+                    style={{ margin:"auto", backgroundColor: "#F9F871" }}
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="strech"
+					
+                >
+					{this.state.recipe.map((results, index) => {
+						return (
+							<Grid item key={results.lname} md={3}>
+								<Card>
+									<CardActionArea onClick={() => this.handleButton(results.steps)}>
+										<CardContent style={{ backgroundColor: "#2F4F4F", textAlign: "center", color: "#ededed" }} >
+                                                                                            <CardMedia
+                                                    style={{
+														height: "150px",
+														margin: "auto",
+														paddingTop: "5%",
+                                                        // margin: "1px",
+                                                        // padding: "auto",
+                                                        borderRadius: "8px",
+                                                        // height: "80px",
+                                                        // width: "80px"
+                                                    }}
+                                                    component="img"
+                                                    image={results.lurl}
+                                                />
+                                            
+											<Typography style={{ fontWeight: "bold" }}>
+												<br/>{results.lname}
+											</Typography>
+											<Typography>
+												Harga : {results.lprice}
+											</Typography>
+										</CardContent>
+									</CardActionArea>
+								</Card>
+							</Grid>
+						);
+					})}
+				</Grid>                
+                <br/><br/><br/><br/>
+			</div>
       </div>
-    </div>
-  );
+            
+		)
+	}
 }
